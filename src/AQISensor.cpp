5c5fe9bbda8187215067b105b7f72049e53969c9
Muhammad Hassaan <hassaanmajeed85@gmail.com>
2025-05-27 11:10:45 +0500

    aht and bmp sensor code update

--- src/AQISensor.cpp
+++ src/AQISensor.cpp
@@ -3,10 +3,7 @@
 // Constructor
 AQISensor::AQISensor() : aqiData { 0.0,
                                    0.0,
-                                   0.0,
-                                   0,
-                                   0,
-                                   0 } {}
+                                   0.0 } {}
 
 // Initialize sensors
 bool AQISensor::init() {
@@ -42,61 +39,6 @@ bool AQISensor::init() {
         log_d("BMP280 initialized");
     }
 
-    Serial2.begin(9600, SERIAL_8N1, PMS_RX_PIN, PMS_TX_PIN);
-    Serial2.write(setPassiveCommand, sizeof(setPassiveCommand));
-    vTaskDelay(1000);
-    
-    uint16_t dummy_pm;
-    pm5007Initialized = readPMS5007(dummy_pm, dummy_pm, dummy_pm);
-    if (pm5007Initialized) {
-        log_d("PM5007 initialized");
-    } else {
-        log_e("PM5007 not detected!");
-    }
-
-    return true;
-}
-
-// Read PM5007 data
-bool AQISensor::readPMS5007(uint16_t &pm1_0, uint16_t &pm2_5, uint16_t &pm10_0) {
-    uint8_t buffer[FRAME_LENGTH];
-    uint16_t checksum = 0;
-
-    while (Serial2.available()) {
-        Serial2.read();
-    }
-
-    Serial2.write(requestDataCommand, sizeof(requestDataCommand));
-    unsigned long startTime = millis();
-    while (Serial2.available() < FRAME_LENGTH) {
-        if (millis() - startTime > 1000) {
-            log_e("Timeout waiting for data!");
-            return false;
-        }
-    }
-
-    for (uint8_t i = 0; i < FRAME_LENGTH; i++) {
-        buffer[i] = Serial2.read();
-    }
-
-    if (buffer[0] != 0x42 || buffer[1] != 0x4D) {
-        log_e("Invalid frame header!");
-        return false;
-    }
-
-    for (uint8_t i = 0; i < FRAME_LENGTH - 2; i++) {
-        checksum += buffer[i];
-    }
-
-    uint16_t receivedChecksum = (buffer[FRAME_LENGTH - 2] << 8) | buffer[FRAME_LENGTH - 1];
-    if (checksum != receivedChecksum) {
-        log_e("Checksum mismatch!");
-        return false;
-    }
-
-    pm1_0 = (buffer[10] << 8) | buffer[11];
-    pm2_5 = (buffer[12] << 8) | buffer[13];
-    pm10_0 = (buffer[14] << 8) | buffer[15];
     return true;
 }
 
@@ -107,9 +49,6 @@ AQIData AQISensor::getData() {
     aqiData.temperature = tempEvent.temperature;
     aqiData.humidity = humidityEvent.relative_humidity;
     aqiData.pressure = bmp.readPressure() / 100.0;
-
-    readPMS5007(aqiData.pm1_0, aqiData.pm2_5, aqiData.pm10_0);
-    
     return aqiData;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -345,10 +345,7 @@ void vAcquireData(void *pvParameters) {
 
             towrite += (aqiSensor.aht20Initialized ? String(aqiData.temperature, 1) : "0") + ", " +
                        (aqiSensor.aht20Initialized ? String(aqiData.humidity, 1) : "0") + ", " +
-                       (aqiSensor.bmp280Initialized ? String(aqiData.pressure, 1) : "0") + ", " +
-                       (aqiSensor.pm5007Initialized ? String(aqiData.pm1_0) : "0") + ", " +
-                       (aqiSensor.pm5007Initialized ? String(aqiData.pm2_5): "0") + ", " +
-                       (aqiSensor.pm5007Initialized ? String(aqiData.pm10_0): "0");
+                       (aqiSensor.bmp280Initialized ? String(aqiData.pressure, 1) : "0");
 
 #ifdef OLED_DISPLAY
             oled_data = true;
